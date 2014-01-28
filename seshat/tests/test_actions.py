470df261a74b17133036660757fd03c22e4eecf1
JoshAshby <joshuaashby@joshashby.com>
2014-01-27 23:03:43 -0700

    more crappy tests

--- seshat/tests/test_actions.py
+++ seshat/tests/test_actions.py
@@ -1,4 +1,5 @@
 import seshat.actions as actions
+from seshat.head import Head
 
 def test_redirect():
     a = actions.Redirect("/")
@@ -6,7 +7,23 @@ def test_redirect():
     assert a.head.status == "303 SEE OTHER"
     assert a.head.headers[0] == ("Location", "/")
 
-def test_note_found():
+def test_not_found():
     a = actions.NotFound()
     assert a.head is not None
     assert a.head.status == "404 NOT FOUND"
+
+def test_unauth():
+    a = actions.Unauthorized()
+    assert a.head is not None
+    assert a.head.status == "401 UNAUTHORIZED"
+
+
+class CustomAction(actions.BaseAction):
+    def __init__(self):
+        self.head = Head("711 WAT")
+
+
+def test_custom():
+    a = CustomAction()
+    assert a.head is not None
+    assert a.head.status == "711 WAT"
