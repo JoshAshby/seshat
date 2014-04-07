f29714274f05b1eec031d0b48aed0fc946c19eaf
JoshAshby <joshuaashby@joshashby.com>
2014-04-06 22:22:37 -0600

    bump version string

--- seshat/__init__.py
+++ seshat/__init__.py
@@ -1,8 +1,3 @@
 #!/usr/bin/env python
-__version__ = '1.0.0'
+__version__ = '2.0.0'
 VERSION = tuple(map(int, __version__.split('.')))
-
-__all__ = ["dispatch",
-           "actions",
-           "request",
-           "route"]
